1bf748eb16e1cc4f978f398bd76c6f6ef0f389ce
Leela Narasimha <leela.narsimha@gmail.com>
2021-03-08 11:10:49 +0530

    React routing

--- react-router-app/src/App.js
+++ react-router-app/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Contact from './pages/Contact/Contact';
+import Posts from './pages/Posts/Posts';
+import Header from './components/Header/Header';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Header />
+            <div className='container'>
+                <Route path='/' component={Home} exact />
+                <Route path='/contact' component={Contact} />
+                <Route path='/posts' component={Posts} />
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
